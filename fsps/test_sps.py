24a0a435ec2de23fba7b4dd0dc198c668f4eb17d
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2016-11-29 19:50:49 -0500

    add docs

--- fsps/test_sps.py
+++ fsps/test_sps.py
@@ -2,6 +2,7 @@
 
 from __future__ import division, print_function
 
+import json
 import numpy as np
 from numpy.testing import assert_allclose
 from .fsps import StellarPopulation
@@ -25,6 +26,18 @@ def _reset_default_params():
 
 
 def compare_reference(ref_in, current):
+    """This method compares the values in a previously generated HDF5 file to
+    the values in a supplied dictionary, and reports the maximum percentage
+    change and where that change occurred.
+
+    :param ref_in:
+        h5py.File object with keys matching those in the given dictionary of values.
+
+    :param current:
+        Dictionary of calculated values that you want to compare to the old
+        version.  Keys are the name of the quentities (e.g. maggies,
+        stellar_mass, etc.)
+    """
     ref_libs = json.loads(ref_in.attrs['libraries'])
     assert ref_libs == libs
 
@@ -42,6 +55,20 @@ def compare_reference(ref_in, current):
 
         
 def test_sps_ssps(reference_in=None, reference_out=None):
+    """This text should say what benchmark you're trying to reproduce,
+    e.g. Conroy & Gunn (2009) Figure 1.  Also use an informative function name.
+
+    :param reference_in: (optional)
+        The name (and path) to a file to use as a refernce for the quantities
+        being calculated.  The currently calculated quantities will be compared
+        to the ones in this file.
+
+    :param reference_out: (optional)
+        Name of hdf5 file to output containing the calculated quantities
+
+    :returns figs:
+        List of matplotlib.pyplot figure objects.
+    """
     _reset_default_params()
 
     # Do stuff to make the data you need
@@ -53,16 +80,19 @@ def test_sps_ssps(reference_in=None, reference_out=None):
     masses = pop.stellar_mass
 
     # Make figures and an output structure
+    # The output structure 
     fig, axes = pl.subplots()
     current = {'ssp_maggies': maggies,
                'stellar_masses': masses}
-    
+
+    # Here we compare to an old set of values stored in an hdf5 file
     if reference_in is not None:
         ref = h5py.File(reference_in, 'r')
         diffstring = compare_reference(ref, current)
         print(diffstring)
         ref.close()
 
+    # Here we write out an hdf5 file
     if reference_out is not None:
         ref = h5py.File(reference_out, 'w')
         for k, v in current.items():
